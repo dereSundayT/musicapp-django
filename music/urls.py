7cf9a73b4cb318ef462ccc2301c4d7034712981f
Sunday Toluwani Dere <toluwanisundaydere@gmail.com>
2020-07-26 14:42:52 +0100

    initail setup completed

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index, name='music-index'),
-    path('<int:pk>/',views.detail,name='music-detail')
-    path('<int:album_id>/favorite/',views.favorite,name='music-favorite')
+    path('', views.index, name='music-index'),
+    path('<int:pk>/', views.detail, name='music-detail'),
+    path('<int:album_id>/favorite/', views.favorite, name='music-favorite')
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
-from .models import Album
+from .models import Album,Song
+
 
 
 def index(request):
@@ -10,6 +11,18 @@ def index(request):
 def detail(request,pk):
     album = get_object_or_404(Album,pk=pk)
     return render(request,'music/detail.html',{'album' : album})
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album,pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError,Song.DoesNotExist):
+         return render(request,'music/detail.html',{'album' : album, 'error_message':'You did not select a valid song'})
+    else:
+        selected_song.is_favorite=True
+        selected_song.save()
+        return render(request,'music/detail.html',{'album' : album})
+
     
 """
 def detail(request,pk):
